02943d5c1be14f4cda18b2a7301a7d60dcdac795
mustafa866 <mhassan@chicagobooth.edu>
2016-05-04 00:25:00 -0500

    create photo complete

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,7 +1,26 @@
 class PhotosController < ApplicationController
-  def index
-    @list_of_photos = Photo.all
-  end
+	def index
+		@list_of_photos = Photo.all
+	end
+
+	def show 
+		@photo = Photo.find_by({ :id => params[:id]})
+
+	end
+
+	def new
+	end
+
+	def create_row
+		p = Photo.new
+		p.caption = params[:the_caption]
+		p.source = params[:the_source]
+		p.save
+
+		redirect_to("http://localhost:3000/photos")
+
+
+	end
 
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
 
-  get("/", { :controller => "photos", :action => "index" })
+	get("/", { :controller => "photos", :action => "index" })
 
-  # Routes to READ photos
-  get("/photos",           { :controller => "photos", :action => "index" })
-  get("/photos/:id",       { :controller => "photos", :action => "show" })
+	#routes to create photos 
+	  get("/photos/new", {:controller => "photos", :action => "new"})
+	  get("/photos/create_photo", {:controller => "photos", :action => "create_row"})
+	# Routes to READ photos
+	  get("/photos",           { :controller => "photos", :action => "index" })
+	  get("/photos/:id",       { :controller => "photos", :action => "show" })
 
 end
