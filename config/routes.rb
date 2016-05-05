21c0333aa6a812daa692bb72425e924efd20aaa0
tburkett88 <airburkett@AirBurketts-MacBook-Air.local>
2016-05-05 00:09:21 -0500

    Unformatted attempt

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,18 @@ Rails.application.routes.draw do
 
   get("/", { :controller => "photos", :action => "index" })
 
+  #create
+  get("/photos/new",           { :controller => "photos", :action => "new_form" })
+  get("/create_photo", { :controller => "photos", :action => "create_row" })
+
+  #edit
+  get("/photos/:id/edit", { :controller => "photos", :action => "edit_form" })
+  get("/update_photo/:id", { :controller => "photos", :action => "update_row" })
+
+
+  #Delete
+  get("/delete_photo/:id", { :controller => "photos", :action => "delete_row" })
+
   # Routes to READ photos
   get("/photos",           { :controller => "photos", :action => "index" })
   get("/photos/:id",       { :controller => "photos", :action => "show" })
