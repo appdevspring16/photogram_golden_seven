3dd6c1a75a1ddfc2daa2922c9fa9f31641644356
Alex Frederick <afreder0@users.noreply.github.com>
2016-05-05 23:23:58 -0500

    stuck

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,5 +3,45 @@ class PhotosController < ApplicationController
     @list_of_photos = Photo.all
   end
 
+  def show
+    @photo = Photo.find_by({ :id => params[:id]})
+  end
+
+  def new_form
+    #code
+  end
+
+  def creat_row
+    #code
+    p = Photo.new
+    p.caption = params[:the_caption]
+    p.source = params[:the_source]
+    p.save
+
+    redirect_to("http://localhost:3000/photos")
+
+    # render("creat_row.html.erb")
+  end
+
+  def destroy
+    d = Photo.find_by({ :id => params[:id]})
+    d.destroy
+    redirect_to("http://localhost:3000/photos")
+  end
+
+  def edit_form
+    p = Photo.find_by({ :id => params[:id]})
+    p.caption = params[:the_caption]
+
+  end
+
+  def update_row
+    p = Photo.find_by({ :id => params[:id]})
+    p.source = params[:the_source]
+    p.caption = params[:the_caption]
+    p.save
+
+    redirect_to("/photos/#{p.id}")
+  end
 
 end
