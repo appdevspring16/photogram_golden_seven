af0e2c8ce80ae0bb0d8def6ea4ae507b139e1911
meghankinney <meghankinney@users.noreply.github.com>
2016-05-06 01:45:41 +0000

    finished video

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -6,9 +6,17 @@ class PhotosController < ApplicationController
   def show
     @photo = Photo.find_by({:id => params[:id]})
   end
-  
+
   def new_form
+  end
+
+  def create_row
+    p = Photo.new
+    p.caption = params[:the_caption]
+    p.source = params[:the_source]
+    p.save
     
+    redirect_to("/photos/")
   end
 
 end
