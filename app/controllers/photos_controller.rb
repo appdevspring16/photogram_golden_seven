bf826a71b6eda79399cdbcb288bf536cd5eb7f73
chrisfcallaway <chrisfcallaway@gmail.com>
2016-05-06 12:27:07 -0500

    callaway-chris-photo-7
    final version, ready for comments

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,5 +3,40 @@ class PhotosController < ApplicationController
     @list_of_photos = Photo.all
   end
 
+def show
+	@photo = Photo.find_by({ :id => params[:id] })
 
+end
+
+def new_form
+
+end
+
+def create_row
+
+	p = Photo.new
+	p.caption = params[:the_caption]
+	p.source = params[:the_source]
+	p.save
+	redirect_to("/photos")
+end
+
+def delete
+	p = Photo.find_by({ :id => params[id] })
+	p.delete
+	redirect_to("/photos")
+end
+
+def edit_form
+	@photo = Photo.find_by({ :id => params[:id] })
+end
+
+def update_row
+	p = Photo.find_by({ :id => params[:id] })
+	p.caption = params[:the_caption]
+	p.source = params[:the_source]
+	p.id = params[:id]
+	p.save
+	redirect_to("/photos/#{p.id}")
+end
 end
