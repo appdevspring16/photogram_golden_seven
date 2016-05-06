9c07f7b8f4a3dfc7912026a33979d32ab1754bb5
Hannah <hannah.meyer@chicagobooth.edu>
2016-05-05 19:49:21 -0500

    Stuck on update_row

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,5 +3,46 @@ class PhotosController < ApplicationController
     @list_of_photos = Photo.all
   end
 
+  def show
+    p = Photo.find_by({ :id => params[:id]})
+    @photo_source = p.source
+    @photo_caption = p.caption
+    @photo_id = p.id
+  end
 
+  def new_form
+
+  end
+
+  def create_row
+    p = Photo.new
+    p.caption = params[:image_caption]
+    p.source = params[:image_url]
+    p.save
+
+    redirect_to("http://localhost:3000/photos")
+  end
+
+  def edit_form
+    p = Photo.find_by({ :id => params[:id]})
+    @photo = p
+
+  end
+
+  def update_row
+    p = Photo.find_by({ :id => params[:id]})
+    p.caption = params[:image_caption]
+    p.source = params[:image_url]
+    p.save
+
+    redirect_to("http://localhost:3000/photos")
+  end
+
+  def are_you_sure
+
+  end
+
+  def delete_photo
+
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,21 @@ Rails.application.routes.draw do
 
   get("/", { :controller => "photos", :action => "index" })
 
+  # Routes to CREATE photos
+  get("/photos/new",       { :controller => "photos", :action => "new_form" })
+  get("/create_photo",     { :controller => "photos", :action => "create_row"})
+
+  #Route to DELETE photos
+  get("/photos/delete",       { :controller => "photos", :action => "are_you_sure" })
+  get("/photos/delete_photo",       { :controller => "photos", :action => "delete_photo" })
+
+  #Routes to EDIT photos
+  get("/photos/:id/edit",  { :controller => "photos", :action => "edit_form"})
+  get("/update_photo/:id",     { :controller => "photos", :action => "update_row"})
+
   # Routes to READ photos
   get("/photos",           { :controller => "photos", :action => "index" })
   get("/photos/:id",       { :controller => "photos", :action => "show" })
+
 
 end
