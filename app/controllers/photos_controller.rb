cede55c86cc663314d00581a94e824d6135a4962
mchen7 <mchen7@chicagobooth.edu>
2016-05-06 11:52:28 -0500

    Final

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -10,5 +10,38 @@ class PhotosController < ApplicationController
     @photo_id = p.id
   end
 
+  def new_form
+
+  end
+
+  def create_row
+    p = Photo.new
+    p.caption = params[:the_caption]
+    p.source = params[:the_source]
+    p.save
+
+    redirect_to("http://localhost:3000/photos")
+  end
+
+  def destroy
+    p = Photo.find_by({ :id => params[:id] })
+    p.destroy
+
+    redirect_to("http://localhost:3000/photos")
+  end
+
+  def edit_form
+    @photo = Photo.find_by({ :id => params[:id] })
+
+  end
+
+  def update_row
+     p = Photo.find_by({ :id => params[:id] })
+     p.caption = params[:the_caption]
+     p.source = params[:the_source]
+     p.save
+
+     redirect_to("/photos/#{p.id}")
+  end
 
 end
