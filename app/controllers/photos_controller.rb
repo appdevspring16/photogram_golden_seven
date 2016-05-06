e5f5d3b03966d2b9f5c8669810336cf3b9a35b9c
Brian Chingono <chingono@chicagobooth.edu>
2016-05-05 22:33:51 -0500

    Finished video and created "delete" functionality

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,5 +3,28 @@ class PhotosController < ApplicationController
     @list_of_photos = Photo.all
   end
 
+def show
+  @photo = Photo.find(params[:id])
+end
+
+def new_form
+
+end
+
+def create_row
+  p = Photo.new
+  p.caption = params[:the_caption]
+  p.source = params[:the_source]
+  p.save
+
+  redirect_to("http://localhost:3000/photos")
+end
+
+def destroy
+  p = Photo.find(params[:id])
+  p.destroy
+
+  redirect_to("http://localhost:3000/photos")
+end
 
 end
