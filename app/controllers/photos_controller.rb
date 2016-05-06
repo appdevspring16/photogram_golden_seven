ffc4ec1aa482068bad2325db55abd09394b772b4
jpark109 <j.park@chicagoboth.edu>
2016-05-06 15:41:25 -0700

    Complete: photogram golden 7 by jay park

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,5 +3,36 @@ class PhotosController < ApplicationController
     @list_of_photos = Photo.all
   end
 
+  def show
+    @photo = Photo.find_by({ :id => params[:id]})
+  end
 
+  def new_form
+    render("new_form.html.erb")
+  end
+
+  def create_row
+    p = Photo.new
+    p.caption = params[:the_caption]
+    p.source = params[:the_source]
+    p.save
+    redirect_to("http://localhost:3000/photos/")
+  end
+
+  def delete
+    p = Photo.find_by({ :id => params[:id]}).destroy
+    redirect_to("http://localhost:3000/photos/")
+  end
+
+  def edit_form
+    @photo = Photo.find_by({ :id => params[:id]})
+  end
+
+  def update_row
+    p = Photo.find_by({ :id => params[:id]})
+    p.caption = params[:the_caption]
+    p.source = params[:the_source]
+    p.save
+    redirect_to("http://localhost:3000/photos/#{params[:id]}")
+  end
 end
