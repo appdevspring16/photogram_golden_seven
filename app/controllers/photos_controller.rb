e165b1ba96009b5a3a181b93350cb46c5c4bc442
Matthew Keenan <mkeenan@chicagobooth.edu>
2016-05-05 21:26:35 -0500

    First pass
    Near fully-functioning

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,5 +3,50 @@ class PhotosController < ApplicationController
     @list_of_photos = Photo.all
   end
 
+  def show
+    @photoid = params[:id]
 
+    @source = Photo.find(@photoid).source
+
+    @caption = Photo.find(@photoid).caption
+  end
+
+  def delete_photo
+    ph = Photo.find(params[:id])
+    ph.destroy
+
+    redirect_to("/photos")
+  end
+
+  def new_form
+
+  end
+
+  def edit_form
+    @photoid = params[:id]
+
+    @source = Photo.find(@photoid).source
+
+    @caption = Photo.find(@photoid).caption
+  end
+
+  def create_row
+    ph = Photo.new
+    ph.source = params[:the_source]
+    ph.caption =params[:the_caption]
+
+    ph.save
+
+    redirect_to("/photos")
+  end
+
+  def update_row
+    ph = Photo.find(params[:id])
+    ph.source = params[:the_source]
+    ph.caption =params[:the_caption]
+
+    ph.save
+
+    redirect_to("/photos")
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,19 @@ Rails.application.routes.draw do
 
   get("/", { :controller => "photos", :action => "index" })
 
+  get("photos/new", { :controller => "photos", :action => "new_form"})
+  get("photos/:id/edit", { :controller => "photos", :action => "edit_form"})
+
   # Routes to READ photos
   get("/photos",           { :controller => "photos", :action => "index" })
   get("/photos/:id",       { :controller => "photos", :action => "show" })
 
+  #Route to add photos
+  get("/create_photo", { :controller => "photos", :action => "create_row"})
+
+  #Route to delete photos
+  get("/delete_photo/:id",       { :controller => "photos", :action => "delete_photo" })
+
+  #Routes to edit photos
+  get("/update_photo/:id", { :controller => "photos", :action => "update_row"})
 end
