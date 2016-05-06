bc2a518bad076e4105daedeb3f0d308f18bfab37
afsaja <asaja@chicagobooth.edu>
2016-05-05 20:03:28 -0500

    Almost there

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,5 +3,35 @@ class PhotosController < ApplicationController
     @list_of_photos = Photo.all
   end
 
+  def show
+    @photo = Photo.find_by({:id => params[:id]})
+  end
+
+  def new_form
+  end
+
+  def create_row
+    p = Photo.new
+    p.caption = params[:the_caption]
+    p.source = params[:the_source]
+    p.save
+    redirect_to("/photos")
+  end
+
+  def delete_row
+    d = Photo.find_by({:id => params[:id]})
+    d.destroy
+    redirect_to("/photos")
+  end
+
+  def edit_form
+    @photo = Photo.find_by({:id => params[:id]})
+    @photo_caption = @photo[:caption]
+    @photo_source = @photo[:source]
+  end
+
+  def edit_row
+
+  end
 
 end
