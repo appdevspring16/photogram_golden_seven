5a0cf663a07c437b8bfb8c5c986ced608289a3b2
Brian Chingono <chingono@chicagobooth.edu>
2016-05-05 23:23:38 -0500

    Near final draft

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -27,4 +27,20 @@ def destroy
   redirect_to("http://localhost:3000/photos")
 end
 
+def edit_form
+  @photo = Photo.find(params[:id])
+  @photo.caption = params[:the_caption]
+  @photo.source = params[:the_source]
+end
+
+def update_row
+  p = Photo.find(params[:id])
+  p.caption = params[:the_caption]
+  p.source = params[:the_source]
+  p.save
+
+  redirect_to("http://localhost:3000/photos")
+end
+
+
 end
