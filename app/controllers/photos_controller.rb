3cdbeb17bef8693ab235887bf912ed13cf59a878
bradley-clarke <bclarke1@chicagobooth.edu>
2016-05-05 22:57:28 -0500

    interim commit

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,5 +3,37 @@ class PhotosController < ApplicationController
     @list_of_photos = Photo.all
   end
 
+  def show
+    p = Photo.find_by({ :id => params[:id] })
+    @photo_source = p.source
+    @photo_caption = p.caption
+    @photo_id = p.id
+
+  end
+
+  def new_form
+
+  end
+
+  def create_row
+    r = Photo.new
+    r.caption = params[:the_caption]
+    r.source = params[:the_source]
+    r.save
+    redirect_to("http://localhost:3000/photos")
+  end
+
+def delete
+    d = Photo.find_by({ :id => params[:id] })
+    d.destroy
+
+redirect_to("http://localhost:3000/photos")
+end
+def edit_form
+  e = Photo.find_by({ :id => params[:id] })
+  @photo_source_e = e.source
+  @photo_caption_e = e.caption
+  @photo_id_e = e.id
+end
 
 end
