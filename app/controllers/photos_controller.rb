59eaed2ebd79678b019128514ba616116e546806
Zachary C. Cornett <zcornett@chicagobooth.edu>
2016-05-06 12:07:03 -0500

    Added Create needs more work
    Still need to save the db

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -4,18 +4,25 @@ class PhotosController < ApplicationController
   end
 
 
-def new_form
-  
+  def new_form
+    # no actuall processing
+  end
 
-end
+  def create_row
 
+    p = Photo.new
+    p.caption = params[:the_caption]
+    p.caption = params[:the_source]
+    p.save
+
+  end
 
 
   def show
     @photo = Photo.find_by ({ :id=> params[:id]})
   end
 
-#DRY it up (Pre-DRYing)
+  #DRY it up (Pre-DRYing)
   # def show
   #   p = Photo.find_by ({ :id=> params[:id]})
   #   @photo_source = p.source
@@ -23,19 +30,19 @@ end
   #   @photo_id = p.id
   # end
 
-# Now the controller needs to know how to handle each variable
-# def show
-#   @photo.source
-#   @photo.caption
-#   @photo.id
-#   render ("show.html.erb")
-# end
+  # Now the controller needs to know how to handle each variable
+  # def show
+  #   @photo.source
+  #   @photo.caption
+  #   @photo.id
+  #   render ("show.html.erb")
+  # end
 
 
-# This is the basic function of the "Show" action. After the hmtl portion has been created extract all of the variables and copy them here
-# def show
-#   render ("show.html.erb")
-# end
+  # This is the basic function of the "Show" action. After the hmtl portion has been created extract all of the variables and copy them here
+  # def show
+  #   render ("show.html.erb")
+  # end
 
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,11 @@ Rails.application.routes.draw do
 
   get("/", { :controller => "photos", :action => "index" })
 
-
+# Routes to Create photos
+# The first route simply produces the blank form.
+# The second route will process and input the data in to the database.
   get("/photos/new",       { :controller => "photos", :action => "new_form" })
-
+  get("/photos/create_photo",       { :controller => "photos", :action => "create_row" })
 
   # Routes to READ photos
   get("/photos",           { :controller => "photos", :action => "index" })
