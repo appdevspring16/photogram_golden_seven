7e3eb40ee984aa3e08a13c4254a9d153d636f365
Jesse C. Taylor <Jesse.Taylor@ChicagoBooth.edu>
2016-05-07 11:03:09 -0500

    Jesse-Taylor

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,7 +1,40 @@
 class PhotosController < ApplicationController
+
   def index
     @list_of_photos = Photo.all
   end
 
+  def show
+    @photo = Photo.find_by({:id => params[:id]})
+  end
 
+  def new_form
+
+    render("new_form.html.erb")
+  end
+
+  def create_row
+    p = Photo.new
+    p.caption = params[:caption]
+    p.source = params[:source]
+    p.save
+    redirect_to("http://localhost:3000")
+  end
+
+  def delete_row
+    x = Photo.find_by({:id => params[:id]})
+    x.destroy
+    redirect_to("http://localhost:3000")
+  end
+
+  def edit_form
+    render("edit_form.html.erb")
+  end
+
+  def update_row
+p = Photo.find_by({ :id => params[:id]})
+p.source = params[:the_source]
+p.caption = params[:the_caption]
+p.save
+redirect_to("/photos/:id")
 end
