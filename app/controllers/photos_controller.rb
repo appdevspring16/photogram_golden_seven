8acf60e255ee8e6437ac07c5d48a7fdf02f69e4c
Skip Heaver <sheaver@chicagobooth.edu>
2016-05-07 09:44:25 -0500

    first commit
    completed all except edit

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,5 +3,37 @@ class PhotosController < ApplicationController
     @list_of_photos = Photo.all
   end
 
+  def show
+    @photo=Photo.find_by({:id=>params[:id]})
+  end
+
+  def create_row
+    p=Photo.new
+    p.caption=params[:the_caption]
+    p.source=params[:the_source]
+    p.save
+    redirect_to("http://localhost:3000/photos")
+  end
+
+  def destroy
+    p=Photo.find_by({:id=>params[:id]})
+    p.destroy
+    redirect_to("http://localhost:3000/photos")
+  end
+
+  def edit_photo
+    p=Photo.find_by({:id=>params[:id]})
+    @caption=p.caption
+    @source=p.source
+  end
+
+  def update_photo
+    p=Photo.find_by({:id=>params[:id]})
+    p.caption=params[:the_caption]
+    p.source=params[:the_source]
+    p.save
+    redirect_to("http://localhost:3000/photos")
+  end
+
 
 end
