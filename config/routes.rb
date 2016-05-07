f295931e0d25905b2e986e6d38f27e4e004089c7
deepeshg <deepesh.goyal@gmail.com>
2016-05-07 12:33:07 -0400

    Complete

--- config/routes.rb
+++ config/routes.rb
@@ -2,19 +2,21 @@ Rails.application.routes.draw do
 
   get("/", { :controller => "photos", :action => "index" })
 
-  # Routes to READ photos
-  get("/photos",           { :controller => "photos", :action => "index" })
-  get("/photos/:id",       { :controller => "photos", :action => "show" })
 
 # Additional routes
   get("/photos/new", { :controller => "photos", :action => "new_form"})
   get("/create_photo", { :controller => "photos", :action => "create_row"})
+
+# why do these need to be above show?
 
   get("/delete_photo/:id", { :controller => "photos", :action => "destroy" })
 
   get("/photos/:id/edit", { :controller => "photos", :action => "edit_form" })
   get("/update_photo/:id", {:controller => "photos", :action => "update_row"})
 
+  # Routes to READ photos
+  get("/photos",           { :controller => "photos", :action => "index" })
+  get("/photos/:id",       { :controller => "photos", :action => "show" })
 
 
 end
