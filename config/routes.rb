cbf6acef6bfe57c35c9814ebeeda3db2c20b5c86
Andrea Ramirez <aramirez@users.noreply.github.com>
2016-05-13 10:55:41 -0500

    Up to destroy rcav

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,17 @@ Rails.application.routes.draw do
 
   get("/", { :controller => "photos", :action => "index" })
 
+# Routes to add a new photo
+  get("/photos/new",          { :controller => "photos", :action => "new_form" })
+  get("/create_photo", { :controller => "photos", :action => "create_row" })
+
   # Routes to READ photos
   get("/photos",           { :controller => "photos", :action => "index" })
   get("/photos/:id",       { :controller => "photos", :action => "show" })
+
+
+# Route to delete photo
+  get("/delete_photo/:id", { :controller => "photos", :action => "destroy" })
+
 
 end
